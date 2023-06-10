503fa909e4653d055ccf70f5057d2a124b13c728
Alexxxandr83 <128635699+Alexxxandr83@users.noreply.github.com>
2023-06-10 14:14:48 +0300

    Update create_tables.sql
    add код для создания таблиц

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+DROP TABLE IF EXISTS employees, customers_data, orders_data;
+
+create table employees
+(
+	employee_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+	title text,
+	birth_date INT NOT NULL,
+	notes text	
+);
+
+CREATE TABLE customers_data (
+customer_id varchar(100) NOT NULL,
+company_name varchar(100) NOT NULL,
+contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders_data (
+order_id INT REFERENCES employees(employee_id) NOT NULL,
+customer_id VARCHAR(100) NOT NULL,
+employee_id int NOT NULL,
+order_date date,
+ship_city VARCHAR(100) NOT NULL
