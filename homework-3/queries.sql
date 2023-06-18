9cec6a5002a6b8bb63a1d22350c2e12385e22f21
Alexxxandr83 <128635699+Alexxxandr83@users.noreply.github.com>
2023-06-18 13:13:32 +0300

    Update queries.sql

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,6 +2,12 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT company_name, CONCAT(employees.first_name, ' ', employees.last_name) as full_name
+FROM customers
+JOIN employees USING(city)
+JOIN orders USING(customer_id)
+WHERE city = 'London'
+
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
@@ -30,3 +36,11 @@ WHERE orders.customer_id IS NULL
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+	
+SELECT DISTINCT product_name
+FROM products
+WHERE product_id IN (
+    SELECT product_id
+    FROM order_details
+    WHERE quantity = 10)
+
